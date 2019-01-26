20397a68d3ff0d991537a92bc273863a75dee3af
Hollowriter <deadgamergx2@hotmail.com.ar>
2019-01-26 10:47:48 -0300

    Movimiento del raton hecho

---
+++ Assets/Scripts/Mice.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mice : MonoBehaviour
+{
+    Transform playerTransform;
+    Vector3 movementVector;
+    int x;
+    int y;
+
+    void Start()
+    {
+        playerTransform = GetComponent<Transform>();
+        movementVector = new Vector3();
+        x = 0;
+        y = 0;
+    }
+
+    void Update()
+    {
+        x = 0;
+        y = 0;
+        if (Input.GetKey(KeyCode.W))
+        {
+            x = 0;
+            y = 5;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            x = 0;
+            y = -5;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            x = -5;
+            y = 0;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            x = 5;
+            y = 0;
+        }
+        movementVector.x = x;
+        movementVector.y = y;
+        playerTransform.position += movementVector * Time.deltaTime;
+    }
+}
