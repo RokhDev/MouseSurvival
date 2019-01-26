282d51b73aad0e0a593b951caad395438d85a078
Hollowriter <deadgamergx2@hotmail.com.ar>
2019-01-26 16:14:23 -0300

    condicion de victoria hecha

---
+++ Assets/Scripts/FoodCount.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodCount : MonoBehaviour
+{
+    GameObject[] findCheese;
+    int foodFound;
+    int loopFound;
+
+    void Start()
+    {
+        findCheese = GameObject.FindGameObjectsWithTag("Cheese");
+        loopFound = 0;
+        for (int i = 0; i < findCheese.Length; i++)
+        {
+            if (findCheese[i].activeSelf)
+            {
+                loopFound++;
+            }
+        }
+        foodFound = loopFound;
+    }
+
+    void Update()
+    {
+        loopFound = 0;
+        for (int i = 0; i < findCheese.Length; i++)
+        {
+            if (findCheese[i].activeSelf)
+            {
+                loopFound++;
+            }
+        }
+        foodFound = loopFound;
+        if (foodFound <= 0)
+        {
+            Debug.Log("Victory");
+        }
+    }
+}
