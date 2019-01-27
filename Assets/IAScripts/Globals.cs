73c93ba3ff87840271ea787bfb8aee3c0593e1f6
Hollowriter <deadgamergx2@hotmail.com.ar>
2019-01-27 13:57:20 -0300

    audioListeners puestos

--- Assets/IAScripts/Globals.cs
+++ Assets/IAScripts/Globals.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Globals : MonoBehaviour {
 
+    public AudioClip sound1;
+
+    public static AudioClip sound1S;
     public static GameObject player;
 
     private void Start()
     {
         player = GameObject.FindWithTag("Mice");
+        sound1S = sound1;
     }
 }
